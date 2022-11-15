b60d97f63a6716d88d5d9e0c6ee1bf86f41d0b97
ReTTy982 <polekpacz@gmail.com>
2022-11-15 15:16:10 +0100

    Prawie do oddania juz

--- Karp/main.py
+++ Karp/main.py
@@ -2,60 +2,59 @@ import itertools
 import time
 import csv
 
-
-def config_import(file):
-    with open(f"{file}", 'r') as f:
+# Czytanie pliku ini
+def get_ini():
+    tsp = {}
+    with open("config.ini", 'r') as f:
         t = int(f.readline().strip())
-        l = [[int(num) for num in line.strip().split(' ')] for line in f]
-    return l
+        for i in range(t):
+            x = f.readline().strip().split(" ")
+            tsp[x[0]] = x[1]
 
+        output = f.readline().strip()
+
+    return tsp, output
+
+# Czytanie plikow txt
 def better_config(file):
     with open(f"{file}", 'r') as f:
         t = int(f.readline().strip())
-        l=[]
+        l = []
         for i in range(t):
             l.append([])
         row = 0
-        column =0
+        column = 0
         liczba = ""
         read = f.read()
         for i in read:
             if i == " " or i == "\n":
                 l[row].append(int(liczba))
-                liczba=""
-                column+=1
+                liczba = ""
+                column += 1
                 if column == t:
                     column = 0
-                    row+=1
+                    row += 1
             else:
-                liczba +=i
+                liczba += i
         return l
-        
+
+
+"""
+1. Wczytaj macierz
+2. Zainicjuj pierwsze podzbiory
+3. Zacznij iterować zbuory od najmniejszego do najwiekszego
+4. Ustaw maske kazdego zbioru
+5. Do każdego podzbioru oblicz najkrotsza droge korzystajac z poprzednich podzbiorow.
+"""
 
 
 def karp(matrix):
-    """
-        matrix = [
-            [0, 20, 30, 31, 28, 40],
-            [30, 0, 10, 14, 20, 44],
-            [40, 20,  0, 10, 22, 50],
-            [41, 24, 20, 0, 14, 42],
-            [38, 30, 32, 24, 0, 28],
-            [50, 54, 60, 52, 38, 0]]
-
-        matrix = [
-            [0  2  9 10],
-            [1  0  6  4],
-            [7, 8, 0, 9],
-            [10, 8, 6, 0]
-        ]
-
-    """
     n = len(matrix[0])
-    dict = {}
+    subset_data = {}
 
-    for k in range(1, n):
-        dict[(1 << k, k)] = (matrix[0][k], 0)
+    # Mapowanie pierwszych zbiorów. Format { (bity, wierzchołek) : (koszt, poprzednik) }
+    for vertex in range(1, n):
+        subset_data[(1 << vertex, vertex)] = (matrix[0][vertex], 0)
 
     for subset_size in range(2, n):
         for subset in itertools.combinations(range(1, n), subset_size):
@@ -66,33 +65,31 @@ def karp(matrix):
             for i in subset:
 
                 prev = bits & ~(1 << i)
-
-                temp = []
+                # wyszukanie poprzednich podzbiorow i wybranie minimalnego
+                patch_cost = []
                 for j in subset:
                     if j == 0 or j == i:
                         continue
-                    temp.append((dict[(prev, j)][0] + matrix[j][i], j))
+                    patch_cost.append(
+                        (subset_data[(prev, j)][0] + matrix[j][i], j))
 
-                dict[(bits, i)] = min(temp)
-
-        # dictprev.update({format(bits, 'b'): format(prev, 'b')})
+                subset_data[(bits, i)] = min(patch_cost)
 
     # Usuwamwierzchołek zero bo jest to wierzchołek startowy
-    bits = (2**n-1) - 1
-    # Tutaj sprawdzam przedostatni wierzcholek
+    bits = (2**n-1) - 1  # = n=6 to bits  = 111110
+    
 
-    temp = []
+    patch_cost = []
     for k in range(1, n):
+        patch_cost.append((subset_data[(bits, k)][0] + matrix[k][0], k))
 
-        temp.append((dict[(bits, k)][0] + matrix[k][0], k))
-
-    opt, parent = min(temp)
+    opt, parent = min(patch_cost)
 
     path = []
     for i in range(n-1):
         path.append(parent)
         new_bits = bits & ~(1 << parent)
-        dummy, parent = dict[(bits, parent)]
+        dummy, parent = subset_data[(bits, parent)]
         bits = new_bits
 
     path.append(0)
@@ -100,13 +97,11 @@ def karp(matrix):
     path.append(0)
     return opt, path
 
-# Back tracking
-
 
 def menu():
     print("""
-    1. TSP - poprawnosc algorytmu.
-    2. TSP - pomiar czasu wykonania algorytmu
+    1. TSP - Wczytaj pojedynczy plik
+    2. TSP - Uruchom program przez config.ini
     """)
     user_choice = input()
     global nodes
@@ -115,78 +110,24 @@ def menu():
         case "1":
             file = input("Podaj nazwe pliku (wraz z .txt)\n")
             nodes = better_config(file)
-            print(nodes)
             cost, route = karp(nodes)
             print(f"Route: {route}, Suma: {cost}\n")
             menu()
         case "2":
-            f = open("wyniki.csv", 'w')
-            writer = csv.writer(f,delimiter=";")
-            writer.writerow(["Plik","Czas[s]","Sciezka","Koszt"])
+            files, output = get_ini()
+            f = open(output, 'w')
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(["Plik", "Czas[s]", "Sciezka", "Koszt"])
 
-            # 6
-            nodes = config_import("6_1.txt")
-            data,cost,route = benchmark(100)
-            writer.writerow(["6_1.txt", data,route,cost])
-            print("Done: 6")
-
-            # 10
-            nodes = config_import("10.txt")
-            data,cost,route = benchmark(100)
-            writer.writerow(["10.txt", data,route,cost])
-            print("Done: 10")
-            # 12
-            nodes = config_import("12.txt")
-            data,cost,route = benchmark(100)
-            writer.writerow(["12.txt", data,route,cost])
-            print("Done: 12")
-
-            # 13
-            nodes = config_import("13.txt")
-            data,cost,route = benchmark(100)
-            writer.writerow(["13.txt", data,route,cost])
-            print("Done: 13")
-
-            # 14
-            nodes = config_import("14.txt")
-            data,cost,route = benchmark(100)
-            writer.writerow(["14.txt", data,route,cost])
-            print("Done: 14")
-
-            #15 
-            nodes = config_import("15.txt")
-            data,cost, route = benchmark(100)
-            writer.writerow(["15.txt", data,route,cost])
-            print("Done: 15")
-
-            #17
-            nodes = better_config("17.txt")
-            data,cost, route = benchmark(100)
-            writer.writerow(["17.txt", data,route,cost])
-            print("Done: 17")
-
-            #21
-            nodes = better_config("21.txt")
-            data,cost,route = benchmark(1)
-            writer.writerow(["21.txt", data,route,cost])
-            print("Done: 21")
-        case "3":
-            #21
-            f = open("wyniki.csv", 'w')
-            writer = csv.writer(f,delimiter=";")
-            writer.writerow(["Plik","Czas[s]","Sciezka","Koszt"])
-            nodes = better_config("21.txt")
-            data,cost,route = benchmark(1)
-            writer.writerow(["21.txt", data,route,cost])
-            print("Done: 21")
+            for i in files.keys():
+                nodes = better_config(i)
+                data, cost, route = benchmark(int(files[i]))
+                writer.writerow([i, data, route, cost])
+                print(f"Done: {i}")
         case _:
-            file = "6_1.txt"
-            nodes = better_config(file)
-            routes = [0, 999999]
-            cost, route = karp(nodes)
-            print(f"Route: {route}, Suma: {cost}\n")
             menu()
 
+# Liczenie czasu
 def benchmark(sample):
     data = 0
     for i in range(sample):
@@ -198,14 +139,6 @@ def benchmark(sample):
 
 
 if __name__ == '__main__':
+    get_ini()
     menu()
-    input("NIE KLIKAJ NICZEGO")
-
-"""
- 0 20 30 31 28 40
-30  0 10 14 20 44
-40 20  0 10 22 50
-41 24 20  0 14 42
-38 30 32 24  0 28
-50 54 60 52 38  0
-"""
+    input("Zakończono działanie programu")
